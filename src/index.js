a15b57d8e5eabc8c715b0cac094b6a74b5d3505e
Jayantilal Prajapat <jayanti.prajapat@gmail.com>
2024-03-30 00:29:16 +0530

    fix abc

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+    const abc = '';
+    console.log(abc);
     return element;
 }
 
